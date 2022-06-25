f814bc8481445a452ef2bfda6d1ad5132976965f
Ramb Memburg <heisramby@gmail.com>
2022-06-25 18:45:54 -0500

    batch improvements

--- erebus.bat
+++ erebus.bat
@@ -5,7 +5,10 @@
 ::    can grab the input file, this is because it is not
 ::    possible for MATLAB to receive get the arguments
 ::    from the terminal.
-:: 3. Finally execute the MATLAB command.
+:: 3. Execute the MATLAB command.
+:: 4. Delete 'config.json' file.
 @echo off
 echo { ^"input^": ^"%1^", ^"output^": ^"%2^", ^"iterations^": %3 } > ./config.json
+mkdir outputs
 matlab -batch "index; exit"
+del config.json
--- src/Erebus.m
+++ src/Erebus.m
@@ -1,29 +1,33 @@
 classdef Erebus
     methods (Static, Access = private)
         function createKey(x, y, iterations)
+            % Create a Mx3 matrix, full of zeros.
             key = zeros(iterations, 3);
-            type = randi([0 1], iterations, 1);
+
+            % Assign random numbers between 0 and 1,
+            % in which 0 represents a row, and 1
+            % represents a column.
+            key(:, 1) = randi([0 1], iterations, 1);
 
             % 0 - rows
             % 1 - columns
             for i = 1:iterations
-                switch type(i, 1)
-                case 0
-                    randRow = randi([1 x], iterations, 1);
-                    randMvmts = randi([-x x], iterations, 1);
-                    key(i, 2) = randRow(1, 1);
-                    key(i, 3) = randMvmts(1, 1);
-                case 1
-                    randColumn = randi([1 y], iterations, 1);
-                    randMvmts = randi([-y y], iterations, 1);
-                    key(i, 2) = randColumn(1, 1);
-                    key(i, 3) = randMvmts(1, 1);
-                otherwise
-                    error("Unexpeceted error while creating encryption key")
+                switch key(i, 1)
+                    case 0
+                        randRow = randi([1 x], iterations, 1);
+                        randMvmts = randi([-x x], iterations, 1);
+                        key(i, 2) = randRow(1, 1);
+                        key(i, 3) = randMvmts(1, 1);
+                    case 1
+                        randColumn = randi([1 y], iterations, 1);
+                        randMvmts = randi([-y y], iterations, 1);
+                        key(i, 2) = randColumn(1, 1);
+                        key(i, 3) = randMvmts(1, 1);
+                    otherwise
+                        error("Unexpeceted error while creating encryption key");
                 end
             end
 
-            key(:, 1) = type;
             date = datestr(now,'yy_mm_dd_HH_MM_SS');
             writematrix(key, "./outputs/" + date + ".csv");
         end
@@ -32,7 +36,7 @@ classdef Erebus
     methods (Static)
         function protect()
             tic;
-            Erebus.createKey(2, 100, 10);
+            Erebus.createKey(2, 100, 800);
             toc;
         end
     end
